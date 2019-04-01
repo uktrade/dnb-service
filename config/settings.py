7601ea38d74b679144ef1edc9a7687b0f1d9d912
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-04-01 23:20:56 +0100

    Add a custom user model

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'dnb',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'user.User'
