58036147ee252cf6368f9544f58e512c306580dd
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-08-28 13:20:32 +0100

    Add detail view HTML format the json responses

--- console/urls.py
+++ console/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SearchView
+from .views import SearchView, DetailView
 
 app_name = 'console'
 
 urlpatterns = [
     path('', SearchView.as_view(), name='search'),
+    path('detail/<int:duns>/', DetailView.as_view(), name='detail'),
 ]
--- console/views.py
+++ console/views.py
@@ -1,8 +1,10 @@
-import json
+import requests
+from json2html import json2html
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import FormView
+from django.views.generic.base import TemplateView
 
 from dnb_api.client import api_request, DNB_COMPANY_SEARCH_ENDPOINT
 from .forms import SearchForm
@@ -14,14 +16,55 @@ class SearchView(FormView):
     template_name = 'search.html'
 
     def form_valid(self, form):
-        response = api_request('POST', DNB_COMPANY_SEARCH_ENDPOINT, json=form.cleaned_data)
 
-        if response.status_code == 200:
-            results = json.dumps(response.json()['searchCandidates'], indent=4)
-        elif response.status_code == 404:
-            results = 'No matching results. Try again.'
+        try:
+            response = api_request('POST', DNB_COMPANY_SEARCH_ENDPOINT, json=form.cleaned_data)
+
+        except requests.exceptions.HTTPError as err:
+            if err.response.status_code == 404:
+                results = 'No matching results. Try again.'
+            else:
+                raise
+        else:
+            results = [{'raw': result['organization'], 'html': json2html.convert(result['organization'])}
+                       for result in response.json()['searchCandidates']]
 
         context = self.get_context_data()
         context['results'] = results
 
         return self.render_to_response(context)
+
+
+@method_decorator(login_required, name='dispatch')
+class DetailView(TemplateView):
+    template_name = 'detail.html'
+
+    def get(self, request, duns, *args, **kwargs):
+        #api_endpoint = f'https://plus.dnb.com/v1/data/duns/{duns}?productId=cmptcs&versionId=v1'
+        api_endpoint = f'https://plus.dnb.com/v1/data/duns/{duns}?productId=cmpelk&versionId=v2'
+
+        try:
+            response = api_request('GET', api_endpoint)
+
+        except requests.exceptions.HTTPError as err:
+            import pdb; pdb.set_trace()
+            if err.response.status_code == 404:
+                results = 'No matching results.'
+            else:
+                raise
+        except Exception as ex:
+            import pdb; pdb.set_trace()
+        else:
+            data = response.json()['organization']
+            result = {
+                'raw': data,
+                'html': json2html.convert(data, clubbing=False)
+            }
+
+        context = self.get_context_data(**kwargs)
+        context['result'] = result
+
+        return self.render_to_response(context)
+
+
+
