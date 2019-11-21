1ca161a312492a27f721874f37c14c1cbbd4e659
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-11-21 14:30:34 +0000

    Add extra company fields

---
+++ company/serialisers.py
@@ -0,0 +1,81 @@
+from rest_framework import serializers
+
+from company.models import Company, IndustryCode, PrimaryIndustryCode, RegistrationNumber
+
+
+class RegistrationNumberSerialiser(serializers.ModelSerializer):
+
+    class Meta:
+        model = RegistrationNumber
+        read_only = True
+        exclude = ['id']
+
+
+class IndustryCodeSerialiser(serializers.ModelSerializer):
+
+    class Meta:
+        model = IndustryCode
+        read_only = True
+        exclude = ['id']
+
+
+class PrimaryIndustryCodeSerialiser(serializers.ModelSerializer):
+
+    class Meta:
+        model = PrimaryIndustryCode
+        read_only = True
+        exclude = ['id']
+
+
+class CompanySerialiser(serializers.ModelSerializer):
+    address_country = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='iso_alpha2'
+    )
+
+    registered_address_country = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='iso_alpha2'
+    )
+
+    registration_numbers = RegistrationNumberSerialiser(many=True)
+    industry_codes = IndustryCodeSerialiser(many=True)
+    primary_industry_codes = PrimaryIndustryCodeSerialiser(many=True)
+
+    class Meta:
+        model = Company
+        fields = [
+            'duns_number',
+            'global_ultimate_duns_number',
+            'primary_name',
+            'global_ultimate_primary_name',
+            'trading_names',
+            'domain',
+            'address_line_1',
+            'address_line_2',
+            'address_town',
+            'address_county',
+            'address_country',
+            'address_postcode',
+            'registered_address_line_1',
+            'registered_address_line_2',
+            'registered_address_town',
+            'registered_address_county',
+            'registered_address_country',
+            'registered_address_postcode',
+            'line_of_business',
+            'is_out_of_business',
+            'year_started',
+            'global_ultimate_duns_number',
+            'employee_number',
+            'is_employees_number_estimated',
+            'annual_sales',
+            'annual_sales_currency',
+            'is_annual_sales_estimated',
+            'legal_status',
+            'registration_numbers',
+            'primary_industry_codes',
+            'industry_codes',
+        ]
---
+++ company/tests/test_serialisers.py
@@ -0,0 +1,137 @@
+from collections import OrderedDict
+
+import pytest
+
+from company.constants import RegistrationNumberChoices
+from company.models import Country
+from company.serialisers import CompanySerialiser
+from .factories import CompanyFactory, IndustryCodeFactory, PrimaryIndustryCodeFactory, RegistrationNumberFactory
+
+
+@pytest.mark.django_db
+def test_company_serialiser():
+    company = CompanyFactory(**{
+        'duns_number': '123456789',
+        'primary_name': 'Test Company 1',
+        'trading_names': ['ACME trading corp'],
+        'global_ultimate_duns_number': '888888888',
+        'global_ultimate_primary_name': 'global primary name',
+        'domain': 'www.e-corp.corp',
+        'is_out_of_business': False,
+        'address_line_1': 'The Old Test Mill 1',
+        'address_line_2': '100 Test Rd',
+        'address_town': 'Cheshire',
+        'address_county': 'address county',
+        'address_postcode': 'address postcode',
+        'address_country': Country.objects.get(iso_alpha2='GB'),
+        'registered_address_line_1': 'reg address line 1',
+        'registered_address_line_2': 'reg address line 2',
+        'registered_address_town': 'reg address town',
+        'registered_address_county': 'reg address county',
+        'registered_address_country':  Country.objects.get(iso_alpha2='GB'),
+        'registered_address_postcode': 'reg postcode',
+        'annual_sales': 51806612000,
+        'annual_sales_currency': 'USD',
+        'is_annual_sales_estimated': None,
+        'employee_number': 24,
+        'year_started': 2000,
+        'is_employees_number_estimated': False,
+        'legal_status': 'foreign_company'
+    })
+
+    RegistrationNumberFactory(**{
+        'company': company,
+        'registration_type': RegistrationNumberChoices.uk_vat_number,
+        'registration_number': '12341234',
+    })
+
+    IndustryCodeFactory(**{
+        'company': company,
+        'code': '517919',
+        'description': 'All Other Telecommunications',
+        'typeDescription': 'North American Industry Classification System 2017',
+        'typeDnbCode': 30832,
+        'priority': 2
+    })
+
+    IndustryCodeFactory(**{
+        'company': company,
+        'code': '423690',
+        'description': 'Other Electronic Parts and Equipment Merchant Wholesalers',
+        'typeDescription': 'North American Industry Classification System 2017',
+        'typeDnbCode': 30832,
+        'priority': 1
+    })
+
+    PrimaryIndustryCodeFactory(**{
+        'company': company,
+        'usSicV4': '5065',
+        'usSicV4Description': 'Whol electronic parts/equipment'
+    })
+
+    assert CompanySerialiser(company).data == {
+        'duns_number': '123456789',
+        'primary_name': 'Test Company 1',
+        'trading_names': ['ACME trading corp'],
+        'registration_numbers': [
+            OrderedDict(
+                [
+                    ('registration_type', 'VAT Registration number'),
+                    ('registration_number', '12341234'),
+                    ('company', 1)
+                ]
+            )
+        ],
+        'global_ultimate_duns_number': '888888888',
+        'global_ultimate_primary_name': 'global primary name',
+        'domain': 'www.e-corp.corp',
+        'is_out_of_business': False,
+        'address_line_1': 'The Old Test Mill 1',
+        'address_line_2': '100 Test Rd',
+        'address_town': 'Cheshire',
+        'address_county': 'address county',
+        'address_postcode': 'address postcode',
+        'address_country': 'GB',
+        'registered_address_line_1': 'reg address line 1',
+        'registered_address_line_2': 'reg address line 2',
+        'registered_address_town': 'reg address town',
+        'registered_address_county': 'reg address county',
+        'registered_address_country': 'GB',
+        'registered_address_postcode': 'reg postcode',
+        'annual_sales': 51806612000.0,
+        'annual_sales_currency': 'USD',
+        'is_annual_sales_estimated': None,
+        'employee_number': 24,
+        'is_employees_number_estimated': False,
+        'primary_industry_codes': [
+            OrderedDict([
+                ('usSicV4', '5065'),
+                ('usSicV4Description', 'Whol electronic parts/equipment'),
+                ('company', 1),
+            ])
+        ],
+        'industry_codes': [
+            OrderedDict(
+                [
+                    ('code', '423690'),
+                    ('description', 'Other Electronic Parts and Equipment Merchant Wholesalers'),
+                    ('priority', 1),
+                    ('typeDescription', 'North American Industry Classification System 2017'),
+                    ('typeDnbCode', '30832'), ('company', 1)
+                ]
+            ),
+            OrderedDict(
+                [
+                    ('code', '517919'),
+                    ('description', 'All Other Telecommunications'),
+                    ('priority', 2),
+                    ('typeDescription', 'North American Industry Classification System 2017'),
+                    ('typeDnbCode', '30832'),
+                    ('company', 1)
+                ]
+            )
+        ],
+        'line_of_business': '',
+        'year_started': 2000,
+        'legal_status': 'foreign_company'
+    }
