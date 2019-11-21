6777fc8b4ee7ae3bb5f882492f03aabd6e50ea65
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-11-21 14:37:32 +0000

    Add extra models to company admin

--- company/admin.py
+++ company/admin.py
@@ -1,20 +1,30 @@
 from django.contrib import admin
 
-from .models import Company, Country, RegistrationNumber
+from .models import Company, Country, IndustryCode, PrimaryIndustryCode, RegistrationNumber
 
 
 class RegistrationNumberInline(admin.TabularInline):
     model = RegistrationNumber
 
 
+class IndustryCodeInline(admin.TabularInline):
+    model = IndustryCode
+
+
+class PrimaryIndustryCodeInline(admin.TabularInline):
+    model = PrimaryIndustryCode
+
+
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('duns_number', 'primary_name', 'is_out_of_business', 'created', 'last_updated')
     list_filter = ('is_out_of_business', 'is_employees_number_estimated', 'is_annual_sales_estimated')
-    search_fields = ('duns_number', 'business_name')
+    search_fields = ('duns_number', 'primary_name')
 
     inlines = [
-        RegistrationNumberInline
+        RegistrationNumberInline,
+        IndustryCodeInline,
+        PrimaryIndustryCodeInline,
     ]
 
 
