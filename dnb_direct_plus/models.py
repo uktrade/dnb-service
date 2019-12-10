1cd37c87a56d6871a28689a8b7cdd6cb943c960b
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2019-12-10 16:13:21 +0000

    fixup! Clean up migration history for dnb_direct_plus app

--- dnb_direct_plus/models.py
+++ dnb_direct_plus/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class MonitoringFileRecord(models.Model):
+    """This model is used to track which monitoring update files have been ingested
+    to avoid ingesting the same file twice."""
     timestamp = models.DateTimeField(auto_now_add=True)
-    file_name = models.CharField(max_length=255)
+    file_name = models.CharField(max_length=255, unique=True)
     total = models.PositiveIntegerField()
     failed = models.PositiveIntegerField()
 
