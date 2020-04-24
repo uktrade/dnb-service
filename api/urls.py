fe5c035de4b36bea4ac6dbbee664d63a9363bdeb
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2020-04-24 16:38:28 +0100

    Change URL to remove GET params

--- api/urls.py
+++ api/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('companies/', CompanyUpdatesAPIView.as_view(), name='company-updates'),
     path('companies/search/', DNBCompanySearchAPIView.as_view(), name='company-search'),
     path('change-request/', ChangeRequestAPIView.as_view(), name='change-request'),
-    path('change-request/?duns_number=<duns_number>&status=pending', GetPendingChangeRequestAPIVIew.as_view(), name='get-change-request'),
+    path('change-request/list', GetPendingChangeRequestAPIVIew.as_view(), name='get-change-request'),
     path('investigation/', InvestigationAPIView.as_view(), name='investigation'),
 ]
