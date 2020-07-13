3f28d042a263b74b90da2dd3098a77dd8666694d
Pippo Raimondi <filipporai@gmail.com>
2020-07-13 09:51:21 +0100

    Add migration to update faulty investigation company details telephone numbers

---
+++ company/migrations/0017_update_company_details.py
@@ -0,0 +1,25 @@
+import django.contrib.postgres.fields.jsonb
+from django.db import migrations
+
+def update_company_details_telephone(apps, schema_editor):
+    faulty_string = '\u0003'
+    InvestigationRequest = apps.get_model('company', 'InvestigationRequest')
+    records = InvestigationRequest.objects.all()
+
+    for record in records:
+        if faulty_string in record.company_details['telephone']:
+            details = record.company_details
+            details['telephone'] = details['telephone'].replace(faulty_string, '')
+
+            record.company_details = details
+            record.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('company', '0016_auto_20200430_1332'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_company_details_telephone),
+    ]
