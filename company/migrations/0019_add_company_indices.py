987a9f16cbc0ac9db291fb7c2caae0e915b75cbc
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-11-19 14:48:53 +0000

    Fix pytest bug on py 3.8

--- company/migrations/0019_add_company_indices.py
+++ company/migrations/0019_add_company_indices.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('company', '0016_auto_20200430_1332'),
+        ('company', '0018_update_company_details'),
     ]
 
     operations = [
