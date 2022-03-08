f5dfdae9b52432472b30b3a03c7efdf3de16b7ee
Edmond De Los Reyes <edmond.delosreyes@digital.trade.gov.uk>
2022-03-08 15:15:11 +0000

    DET-77 Added test data and specs coverage

--- conftest.py
+++ conftest.py
@@ -23,11 +23,13 @@ def company_list_api_response_json():
                            'dnb_direct_plus/tests/api_data/company_list_test_response.json')) as f:
         return f.read()
 
+
 @pytest.fixture(scope='module')
 def company_list_v2_api_response_json():
     with open(os.path.join(os.path.dirname(__file__),
                            'dnb_direct_plus/tests/api_data/company_list_v2_test_response.json')) as f:
         return f.read()
+
 
 @pytest.fixture(scope='module')
 def company_list_v2_expected_data_json():
@@ -35,8 +37,16 @@ def company_list_v2_expected_data_json():
                            'dnb_direct_plus/tests/api_data/company_list_v2_populated_region.json')) as f:
         return f.read()
 
+
 @pytest.fixture(scope='module')
 def data_duns_api_response_json():
     with open(os.path.join(os.path.dirname(__file__),
                            'dnb_direct_plus/tests/api_data/data_duns_test_response.json')) as f:
+        return f.read()
+
+
+@pytest.fixture(scope='module')
+def company_by_duns_v1_api_response_json():
+    with open(os.path.join(os.path.dirname(__file__),
+                           'dnb_direct_plus/tests/api_data/company_by_duns_v1_test_response.json')) as f:
         return f.read()
