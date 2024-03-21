78df5e7e7ce88b55d4bb0d9230fe014b7e8e3411
Pippo Raimondi <piporamaa@gmail.com>
2024-03-21 07:27:57 +0000

    Update deprecated function

--- company/models.py
+++ company/models.py
@@ -4,7 +4,7 @@ from django.contrib.postgres.fields import ArrayField, JSONField
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils.timezone import now
 
 from company.constants import (
